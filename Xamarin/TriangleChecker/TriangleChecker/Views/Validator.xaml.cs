6fc56f2090c181e6bdd51c81e7f7f77a53b13ecf
Rodion I <yafarovrs@gmail.com>
2021-02-24 00:21:19 +0100

    Add initial implementation of the Triangle Checker App
    App purpose will be to conclude which type of the triangle it is, based
    on the lengths of the triangle sides.
    
    As of now only UI is ready and no logic yet implemented to provide the
    result.

---
+++ Xamarin/TriangleChecker/TriangleChecker/Views/Validator.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TriangleChecker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Validator: ContentView
+    {
+        public Validator()
+        {
+            InitializeComponent();
+        }
+
+        private void RunButtonClicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void EntryCompleted(object sender, EventArgs e)
+        {
+            try
+            {
+                // Check that event sender is Entry and StackLayout is used
+                if (!(sender is Entry entry) || !(entry.Parent is StackLayout stack))
+                    return;
+                var list = stack.Children;
+                var index = list.IndexOf(entry);
+                var next = list.ElementAt(++index % list.Count);
+                next?.Focus();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception: " + ex.Message);
+            }
+        }
+    }
+}
