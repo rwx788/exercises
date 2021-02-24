32862b5a4c5f836b727b0e35841093e08936fb69
Rodion I <yafarovrs@gmail.com>
2021-02-24 20:57:56 +0100

    Implement Controller to validate triangle type
    Initial implementation to show triangle type using 3 lengths sides.

---
+++ Xamarin/TriangleChecker/TriangleChecker/Controllers/ValidatorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace TriangleChecker.Controllers
+{
+    public enum TriangleTypes { Equilateral, Isosceles, Scalene, None }
+
+    class ValidatorController
+    {
+        public static Enum GetTriangleType(int sideA, int sideB, int sideC) 
+        {
+            if(sideA + sideB <= sideC || sideB + sideC <= sideA || sideC + sideA <= sideB)
+                return TriangleTypes.None;
+
+            if (sideA == sideB && sideB == sideC && sideC == sideA)
+                return TriangleTypes.Equilateral;
+
+            if (sideA == sideB || sideB == sideC || sideC == sideA)
+                return TriangleTypes.Isosceles;
+
+            return TriangleTypes.Scalene;
+        }
+    }
+}
