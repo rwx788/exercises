18ee7dccaddbe26f390bd7f6b8a36178ddc9cb90
Rodion I <yafarovrs@gmail.com>
2021-02-24 22:52:52 +0100

    Add sctructure for the UnitTests

---
+++ Xamarin/TriangleChecker/TriangleChecker.UnitTests/Controllers/ValidatorControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriangleChecker.Controllers.Tests
+{
+    [TestClass()]
+    public class ValidatorControllerTests
+    {
+        [TestMethod()]
+        public void GetTriangleTypeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
