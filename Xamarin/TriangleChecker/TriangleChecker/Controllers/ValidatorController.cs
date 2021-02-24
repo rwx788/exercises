16986408b0cebab272b97ebfab6a6badd6fe1ced
Rodion I <yafarovrs@gmail.com>
2021-02-25 00:11:06 +0100

    Fix overflow when sum of sides exceeds max value
    As an easy solution, using UInt64 to calculate sum of UInt32 numbers.
    In case bigger number has to be supported, we will need to develop
    overflow detection and throw Exception in case sum cannot be calculated
    properly, which should be handled in the View.

--- Xamarin/TriangleChecker/TriangleChecker/Controllers/ValidatorController.cs
+++ Xamarin/TriangleChecker/TriangleChecker/Controllers/ValidatorController.cs
@@ -8,8 +8,16 @@ namespace TriangleChecker.Controllers
     {
         public static Enum GetTriangleType(UInt32 sideA, UInt32 sideB, UInt32 sideC)
         {
-            if (sideA + sideB <= sideC || sideB + sideC <= sideA || sideC + sideA <= sideB)
+            var uint64_sideA = Convert.ToUInt64(sideA);
+            var uint64_sideB = Convert.ToUInt64(sideB);
+            var uint64_sideC = Convert.ToUInt64(sideC);
+
+            if ((uint64_sideA + uint64_sideB).CompareTo(uint64_sideC) <= 0 ||
+                (uint64_sideB + uint64_sideC).CompareTo(uint64_sideA) <= 0 ||
+                (uint64_sideC + uint64_sideA).CompareTo(uint64_sideB) <= 0)
+            {
                 return TriangleTypes.None;
+            }
 
             if (sideA == sideB && sideB == sideC && sideC == sideA)
                 return TriangleTypes.Equilateral;
