bf818dc913ead1163954ab3a128c20a11e9cc75a
Rodion I <yafarovrs@gmail.com>
2021-02-24 23:28:34 +0100

    Add Unit Tests for the ValidatorController
    Tests have identified issues, when using Int32 MaxValue, as we use summ
    to detect if the triangle can be built, so it overflows. This will be
    fixed in the upcoming commit.

--- Xamarin/TriangleChecker/TriangleChecker.UnitTests/Controllers/ValidatorControllerTests.cs
+++ Xamarin/TriangleChecker/TriangleChecker.UnitTests/Controllers/ValidatorControllerTests.cs
@@ -6,9 +6,40 @@ namespace TriangleChecker.Controllers.Tests
     public class ValidatorControllerTests
     {
         [TestMethod()]
-        public void GetTriangleTypeTest()
+        public void GetTriangleTypeTestNone()
         {
-            Assert.Fail();
+            Assert.AreEqual(TriangleTypes.None, ValidatorController.GetTriangleType(0, 0, 0));
+            Assert.AreEqual(TriangleTypes.None, ValidatorController.GetTriangleType(-1, -5, -3));
+            Assert.AreEqual(TriangleTypes.None, ValidatorController.GetTriangleType(1, 5, 3));
         }
+
+        [TestMethod]
+        public void GetTriangleTypeTestEquilateral()
+        {
+            Assert.AreEqual(TriangleTypes.Equilateral, ValidatorController.GetTriangleType(1, 1, 1));
+        }
+
+        [TestMethod]
+        public void GetTriangleTypeTestIsosceles()
+        {
+            Assert.AreEqual(TriangleTypes.Isosceles, ValidatorController.GetTriangleType(4, 3, 3));
+        }
+
+        [TestMethod]
+        public void GetTriangleTypeTestScalene()
+        {
+            Assert.AreEqual(TriangleTypes.Scalene, ValidatorController.GetTriangleType(3, 4, 5));
+        }
+
+        [TestMethod]
+        public void GetTriangleTypeTestMaxValue()
+        {
+            Assert.AreEqual(TriangleTypes.None,        ValidatorController.GetTriangleType(2147483647, 1000000000, 1000000000));
+            Assert.AreEqual(TriangleTypes.Equilateral, ValidatorController.GetTriangleType(2147483647, 2147483647, 2147483647));
+            Assert.AreEqual(TriangleTypes.Isosceles,   ValidatorController.GetTriangleType(2147483647, 13, 2147483647));
+            Assert.AreEqual(TriangleTypes.Isosceles,   ValidatorController.GetTriangleType(2147483647, 2147483647, 2147483647));
+            Assert.AreEqual(TriangleTypes.Scalene,     ValidatorController.GetTriangleType(2147483647, 2147400000, 2147312344));
+        }
+
     }
 }
