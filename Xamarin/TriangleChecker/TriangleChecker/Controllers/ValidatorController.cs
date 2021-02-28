97608768e623648da1d0c3d2d7cf7069987849a2
Rodion I <yafarovrs@gmail.com>
2021-02-28 19:32:41 +0100

    Add documentation to the controller methods
    Documenting ValidatorController class methods using xml comments.

--- Xamarin/TriangleChecker/TriangleChecker/Controllers/ValidatorController.cs
+++ Xamarin/TriangleChecker/TriangleChecker/Controllers/ValidatorController.cs
@@ -5,8 +5,24 @@ namespace TriangleChecker.Controllers
 {
     public enum TriangleTypes { Equilateral, Isosceles, Scalene, None }
 
+
     public class ValidatorController
     {
+        /// <summary>Method evaluates if there is a triangle by given
+        /// length sides. If it the case, type of the triangle is returned.
+        /// Following types are supported: Equilateral, Isosceles, Scalene.
+        /// <example>For example:
+        /// <code>
+        ///    Enum type = ValidatorController.GetTriangleType((UInt32)a, (UInt32)b, (UInt32)c);
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="sideA">Integer value whic denotes length of the side A.</param>
+        /// <param name="sideB">Integer value whic denotes length of the side B.</param>
+        /// <param name="sideC">Integer value whic denotes length of the side C.</param>
+        /// <returns>
+        /// Enum one of the values of <c>TriangleTypes</c> enum.
+        /// </returns>
         public static Enum GetTriangleType(UInt32 sideA, UInt32 sideB, UInt32 sideC)
         {
             var uint64_sideA = Convert.ToUInt64(sideA);
@@ -29,6 +45,17 @@ namespace TriangleChecker.Controllers
             return TriangleTypes.Scalene;
         }
 
+        /// <summary>Method tries to parse input string and return corresponding 
+        /// unsigned integer value. Zero is returned in case given string cannot be parsed.
+        /// See documentation for <c>UInt32.TryParse</c>.
+        /// <example>For example:
+        /// <code>
+        ///    ValidatorController.ParseInput("777");
+        /// </code>
+        /// results in <c>UInt32</c> value 777.
+        /// </example>
+        /// </summary>
+        /// <param name="input">Input string, which we try to parse to unsigned integer.</param>
         public static UInt32 ParseInput(string input)
         {
 
