774759e1bfe8b8666bd89c1916ed0f2dc37ebd90
rwx788 <riafarov@suse.com>
2021-03-22 13:11:16 +0100

    Add k3s dashboard config

---
+++ k3s/dashboard/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+k3s kubectl delete ns kubernetes-dashboard
+GITHUB_URL=https://github.com/kubernetes/dashboard/releases
+VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
+k3s kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml -f dashboard.admin-user.yml -f dashboard.admin-user-role.yml
